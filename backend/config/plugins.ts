dbf924965efa3a53adc7105463b7fc15204b5ca7
nandresgm <33487017+nandresgm@users.noreply.github.com>
2025-02-13 02:04:13 -0500

    ampliar tamaño de archivos

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,7 +1,13 @@
 export default {
-    "users-permissions": {
-      config: {
-        jwtSecret: process.env.JWT_SECRET || 'VsGjnVaDLqKjDwQk3r9ShA==',
-      },
+  "users-permissions": {
+    config: {
+      jwtSecret: process.env.JWT_SECRET || 'VsGjnVaDLqKjDwQk3r9ShA==',
     },
-  };
+  },
+  upload: {
+    provider: 'local', 
+    providerOptions: {
+      sizeLimit: 10000000, 
+    },
+  },
+};
